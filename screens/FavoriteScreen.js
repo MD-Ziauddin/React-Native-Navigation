5a0980a7b13a5a2469e908d30fb4e12f29569a80
MD Ziauddin <md.ziauddin02@gmail.com>
2021-06-05 20:10:31 +0530

    ,,,

---
+++ screens/FavoriteScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const FavoriteScreen = (props) => {
+  <View style={styles.screen}>
+    <Text>The Favorite Screen</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+});
+
+export default FavoriteScreen;
